fee111c17bd5f5c02d55bab3e6a67d7381a1b46f
Daniel Covington <DanielmCovington@gmail.com>
2023-05-25 16:55:46 -0500

    save

--- Develop/controllers/api/index.js
+++ Develop/controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const routineRoutes = require('./routineRoutes');
+const workoutRoutes = require('./workoutRoutes');
 
 router.use('/users', userRoutes);
-router.use('/Routine', projectRoutes);
+router.use('/workout', workoutRoutes);
 
 module.exports = router;
--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,13 +1,13 @@
 const User = require('./User');
-const Project = require('./Project');
+const Project = require('./workout');
 
 User.hasMany(Project, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-Project.belongsTo(User, {
+Workout.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Project };
+module.exports = { User, Workout };
