d173823abf12fc22c8ea76386fb2e10b1f9d8c1c
Annalee Beltran <annalee.beltranusaf@gmail.com>
2023-06-03 16:35:28 -0500

    added youtube api routing and to rendering page/ added README

--- Develop/controllers/index.js
+++ Develop/controllers/index.js
@@ -4,10 +4,12 @@ const userRoutes = require("./api/userRoutes");
 const workoutRoutes = require("./api/workoutRoutes");
 const homeRoutes = require("./homeRoutes");
 const profileRoutes = require("./profileRoutes");
+const youtubeRoutes = require("./api/youtubeRoutes");
 
 router.use("/", homeRoutes);
 router.use("/api/users", userRoutes);
 router.use("/api/workouts", workoutRoutes);
 router.use("/profile", profileRoutes);
+router.use("/api/youtube", youtubeRoutes);
 
 module.exports = router;
