5f493b1167d54062f3073812962cbb5ce2beb7cf
Sam Moore <sammooresemail23@gmail.com>
2023-06-05 21:21:41 -0700

    Heartrate chart functional

--- My_Fitness_Junkie-main/Develop/controllers/api/userRoutes.js
+++ My_Fitness_Junkie-main/Develop/controllers/api/userRoutes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { User } = require("../../models");
 const axios = require("axios").default;
+const heartbeatData = require("../../seeds/heartRate.json");
 
 router.post("/", async (req, res) => {
   try {
@@ -76,5 +77,13 @@ router.put("/update/:id", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+
+// route //
+router.get("/heartbeatData", (req, res) => {
+  res.json(heartbeatData);
+});
+
+
 
 module.exports = router;
--- My_Fitness_Junkie-main/Develop/public/js/chart.js
+++ My_Fitness_Junkie-main/Develop/public/js/chart.js
@@ -5,10 +5,10 @@
 // x axis is a weekly prerecorded set of data//
 const xValues = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 // y axis is a heart rate data set in beats per minute //
-const yValues = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300];
+const yValues = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200];
 
 // week heart rate // 
-function populateHeartChart() {
+window.onload = function populateHeartChart() {
 
   var dataPoints = [];
   
@@ -34,7 +34,9 @@ function populateHeartChart() {
       dataPoints: dataPoints
     }]
   });
-  
+
+
+
   // adds the data to the chart from JSON //
   function addData(data) {
     for (var i = 0; i < data.length; i++) {
@@ -45,40 +47,53 @@ function populateHeartChart() {
     }
     chart.render();
   }
- 
-// Link via Api Call //
-  JSON.parse("My_Fitness_Junkie-main\Develop\seeds\workoutData.json", addData);
-  }
 
-// Heart Rate Chart Functionality //
-new Chart("myChart", {
-  type: "line",
-  data: {
-    // Labels for X & Y Axes //
-    labels: xValues, yValues,
-    datasets: [
-      // Each exercise is a different colored line //
-      // Strength //
-    {
-      data: [],
-      borderColor: "red",
-      fill: false
-    },
-    // Cardio //
-    {
-      data: [],
-      borderColor: "green",
-      fill: false
-    },
-    // Endurance //
-    {
-      data: [],
-      borderColor: "blue",
-      fill: false
-    },
-  ]
-  },
-  options: {
-    legend: {display: false}
-  }
-});
+
+fetch("/api/users/heartbeatData")
+  .then(response => response.json())
+  .then(data => {
+    // Call the addData function with the parsed data
+    console.log(data);
+    addData(data);
+  })
+  .catch(error => {
+    // Handle any errors
+    console.error(error);
+  });
+}
+
+// // Heart Rate Chart Functionality //
+// new Chart("myChart", {
+//   type: "line",
+//   data: {
+//     // Labels for X & Y Axes //
+//     labels: xValues, yValues,
+//     datasets: [
+//       // Each exercise is a different colored line //
+//       // Strength //
+//     {
+//       data: [],
+//       borderColor: "red",
+//       fill: false
+//     },
+//     // Cardio //
+//     {
+//       data: [],
+//       borderColor: "green",
+//       fill: false
+//     },
+//     // Endurance //
+//     {
+//       data: [],
+//       borderColor: "blue",
+//       fill: false
+//     },
+//   ]
+//   },
+//   options: {
+//     legend: {display: false}
+//   }
+// });
+// */
+
+// // My_Fitness_Junkie-main\Develop\seeds\heartRate.json
